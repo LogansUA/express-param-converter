316803e7959254108cb87d8fb46761c977fd6c0a
Oleg Kachinskyi <logansoleg@gmail.com>
2017-09-11 14:29:44 +0300

    Initialize repo

---
+++ dist/index.js
@@ -0,0 +1,110 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+function hasAttribute(request, name) {
+    return typeof request.params[name] !== 'undefined';
+}
+
+function getIdentifier(request, name) {
+    if (hasAttribute(request, name)) {
+        return request.params[name];
+    }
+
+    return false;
+}
+
+function find(model, request, options, name) {
+    var identifier = getIdentifier(request, name);
+
+    if (identifier === false || identifier === null) {
+        return Promise.resolve(false);
+    }
+
+    return new Promise(function (resolve, reject) {
+        return model.findById(identifier).then(function (resource) {
+            if (typeof options.plain !== 'undefined' && options.plain === true) {
+                return resolve(resource.get({ plain: true }));
+            }
+
+            return resolve(resource);
+        }, reject);
+    });
+}
+
+function findOneBy(model, request, options) {
+    var _options$mappings = options.mappings,
+        mappings = _options$mappings === undefined ? {} : _options$mappings;
+
+
+    if (Object.keys(mappings).length === 0) {
+        return Promise.resolve(false);
+    }
+
+    var criteria = {};
+
+    for (var alias in mappings) {
+        var field = mappings[alias];
+
+        var identifier = getIdentifier(request, alias);
+
+        if (identifier) {
+            criteria[field] = identifier;
+        }
+    }
+
+    return new Promise(function (resolve, reject) {
+        return model.findOne({ where: criteria }).then(function (resource) {
+            if (typeof options.plain !== 'undefined' && options.plain === true) {
+                return resolve(resource.get({ plain: true }));
+            }
+
+            return resolve(resource);
+        }, reject);
+    });
+}
+
+function modelParamConverter() {
+    var parameters = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+    var name = parameters.name,
+        model = parameters.model,
+        _parameters$options = parameters.options,
+        options = _parameters$options === undefined ? {} : _parameters$options;
+
+
+    return function (req, res, next) {
+        var isOptional = false;
+
+        if (!model || (typeof model === 'undefined' ? 'undefined' : _typeof(model)) !== 'object') {
+            return next(new Error('Model is not defined!'));
+        }
+
+        if (!hasAttribute(req, name)) {
+            isOptional = true;
+        }
+
+        return find(model, req, options, name).then(function (resource) {
+            return resource || findOneBy(model, req, options);
+        }).then(function (resource) {
+            if (!resource) {
+                if (isOptional) {
+                    throw new Error('Unable to guess how to get a instance from the request information.');
+                } else {
+                    throw new Error(model.toString() + ' not found!');
+                }
+            }
+
+            return resource;
+        }).then(function (resource) {
+            req.params[name] = resource;
+
+            return next();
+        }).catch(next);
+    };
+}
+
+exports.default = modelParamConverter;
---
+++ src/index.js
@@ -0,0 +1,95 @@
+function hasAttribute(request, name) {
+    return typeof request.params[name] !== 'undefined';
+}
+
+function getIdentifier(request, name) {
+    if (hasAttribute(request, name)) {
+        return request.params[name];
+    }
+
+    return false;
+}
+
+function find(model, request, options, name) {
+    const identifier = getIdentifier(request, name);
+
+    if (identifier === false || identifier === null) {
+        return Promise.resolve(false);
+    }
+
+    return new Promise((resolve, reject) => model.findById(identifier)
+        .then((resource) => {
+            if (typeof options.plain !== 'undefined' && options.plain === true) {
+                return resolve(resource.get({ plain: true }));
+            }
+
+            return resolve(resource);
+        }, reject));
+}
+
+function findOneBy(model, request, options) {
+    const { mappings = {} } = options;
+
+    if (Object.keys(mappings).length === 0) {
+        return Promise.resolve(false);
+    }
+
+    const criteria = {};
+
+    for (const alias in mappings) {
+        const field = mappings[alias];
+
+        const identifier = getIdentifier(request, alias);
+
+        if (identifier) {
+            criteria[field] = identifier;
+        }
+    }
+
+    return new Promise((resolve, reject) => model.findOne({ where: criteria })
+        .then((resource) => {
+            if (typeof options.plain !== 'undefined' && options.plain === true) {
+                return resolve(resource.get({ plain: true }));
+            }
+
+            return resolve(resource);
+        }, reject));
+}
+
+function modelParamConverter(parameters = {}) {
+    const { name, model, options = {} } = parameters;
+
+    return (req, res, next) => {
+        let isOptional = false;
+
+        if (!model || typeof model !== 'object') {
+            return next(new Error('Model is not defined!'));
+        }
+
+        if (!hasAttribute(req, name)) {
+            isOptional = true;
+        }
+
+        return find(model, req, options, name)
+            .then(resource => resource || findOneBy(model, req, options))
+            .then((resource) => {
+                if (!resource) {
+                    if (isOptional) {
+                        throw new Error('Unable to guess how to get a instance from the request information.');
+                    } else {
+                        throw new Error(`${model.toString()} not found!`);
+                    }
+                }
+
+                return resource;
+            })
+            .then((resource) => {
+                req.params[name] = resource;
+
+                return next();
+            })
+            .catch(next);
+    };
+}
+
+export { modelParamConverter as default };
